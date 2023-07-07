19bbd8fd9dea8c0f14adbd2512abdeaedd9a5006
Guna Palanivel <111827797+GunaPalanivel@users.noreply.github.com>
2023-07-07 13:05:43 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,403 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Sindhi Biryani',
+    category : 'biryani',
+    rating : 4.7,
+    price: 15,
+    img: 'images/biryani/b1.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Awadhi Biryani',
+    category : 'biryani',
+    rating : 4.1,
+    price: 18,
+    img: 'images/biryani/b2.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 3.3,
+    price: 10,
+    img: 'images/biryani/b3.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Malabar Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/b4.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Omani Biryani',
+    category : 'biryani',
+    rating : 4.4,
+    price: 14,
+    img: 'images/biryani/b5.png',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Kamrupi Biryani',
+    category : 'biryani',
+    rating : 4.8,
+    price: 22,
+    img: 'images/biryani/b6.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 2.3,
+    price: 10,
+    img: 'images/biryani/b7.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Memoni Biryani',
+    category : 'biryani',
+    rating : 4.9,
+    price: 22,
+    img: 'images/biryani/b8.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Thalassery Biryani',
+    category : 'biryani',
+    rating : 2.3,
+    price: 9,
+    img: 'images/biryani/b9.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Tikka Masala',
+    category : 'chicken',
+    rating : 4.4,
+    price: 21,
+    img: 'images/chicken/c1.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Lemon Garlic',
+    category : 'chicken',
+    rating : 3.3,
+    price: 11,
+    img: 'images/chicken/c2.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Parmesan',
+    category : 'chicken',
+    rating : 4.5,
+    price: 19,
+    img: 'images/chicken/c3.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Alfredo',
+    category : 'chicken',
+    rating : 4.6,
+    price: 22,
+    img: 'images/chicken/c4.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Mustard Glazed',
+    category : 'chicken',
+    rating : 3.3,
+    price: 18,
+    img: 'images/chicken/c5.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Spicy Szechuan',
+    category : 'chicken',
+    rating : 3.9,
+    price: 20,
+    img: 'images/chicken/c6.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Paneer Lababdar',
+    category : 'paneer',
+    rating : 3.3,
+    price: 11,
+    img: 'images/paneer/p1.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Paneer Pasanda',
+    category : 'paneer',
+    rating : 2.9,
+    price: 10,
+    img: 'images/paneer/p2.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Paneer Do Pyaza',
+    category : 'paneer',
+    rating : 3.9,
+    price: 16,
+    img: 'images/paneer/p3.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Paneer Makhani',
+    category : 'paneer',
+    rating : 4.9,
+    price: 29,
+    img: 'images/paneer/p4.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Paneer Bhurji',
+    category : 'paneer',
+    rating : 3.3,
+    price: 10,
+    img: 'images/paneer/p5.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 3.9,
+    price: 9,
+    img: 'images/paneer/p6.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Shahi Paneer',
+    category : 'paneer',
+    rating : 4.4,
+    price: 15,
+    img: 'images/paneer/p7.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Spinach Salad',
+    category : 'vegetable',
+    rating : 4.2,
+    price: 9,
+    img: 'images/vegetable/v1.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Quinoa Salad',
+    category : 'vegetable',
+    rating : 4.5,
+    price: 8,
+    img: 'images/vegetable/v2.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Cobb Salad',
+    category : 'vegetable',
+    rating : 4.6,
+    price: 10,
+    img: 'images/vegetable/v3.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Waldorf Salad',
+    category : 'vegetable',
+    rating : 4.9,
+    price: 17,
+    img: 'images/vegetable/v4.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Caesar Salad',
+    category : 'vegetable',
+    rating : 3.3,
+    price: 11,
+    img: 'images/vegetable/v5.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Caprese Salad',
+    category : 'vegetable',
+    rating : 4.5,
+    price: 10,
+    img: 'images/vegetable/v6.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Greek Salad',
+    category : 'vegetable',
+    rating : 3.3,
+    price: 5,
+    img: 'images/vegetable/v7.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Kung Pao',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chinese/h1.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'General Tso',
+    category : 'chinese',
+    rating : 4.4,
+    price: 17,
+    img: 'images/chinese/h2.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Sweet and Sour',
+    category : 'chinese',
+    rating : 4.9,
+    price: 15,
+    img: 'images/chinese/h3.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Mongolian Beef',
+    category : 'chinese',
+    rating : 4.3,
+    price: 12,
+    img: 'images/chinese/h4.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Mapo Tofu',
+    category : 'chinese',
+    rating : 4.9,
+    price: 18,
+    img: 'images/chinese/h5.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Dim Sum',
+    category : 'chinese',
+    rating : 3.3,
+    price: 10,
+    img: 'images/chinese/h6.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Peking Duck',
+    category : 'chinese',
+    rating : 4.6,
+    price: 34,
+    img: 'images/chinese/h7.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Rice',
+    category : 'south indian',
+    rating : 4.8,
+    price: 38,
+    img: 'images/south indian/s1.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Curd Rice',
+    category : 'south indian',
+    rating : 3.9,
+    price: 29,
+    img: 'images/south indian/s2.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Sambar Rice',
+    category : 'south indian',
+    rating : 4.4,
+    price: 32,
+    img: 'images/south indian/s3.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Carrot Rice',
+    category : 'south indian',
+    rating : 4.7,
+    price: 27,
+    img: 'images/south indian/s4.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Plain with leafy greens',
+    category : 'south indian',
+    rating : 4.1,
+    price: 19,
+    img: 'images/south indian/s5.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Tamarind Rice',
+    category : 'south indian',
+    rating : 4.3,
+    price: 30,
+    img: 'images/south indian/s6.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Rasam',
+    category : 'south indian',
+    rating : 3.9,
+    price: 25,
+    img: 'images/south indian/s7.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Kichidi',
+    category : 'south indian',
+    rating : 4.9,
+    price: 35,
+    img: 'images/south indian/s8.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
